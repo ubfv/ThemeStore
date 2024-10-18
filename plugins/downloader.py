07167cb9b89118459f7c42d14da30a4dd963dba7
amir <119237819+ubfv@users.noreply.github.com>
2024-10-18 15:15:32 +0300

    Update downloader.py

--- plugins/downloader.py
+++ plugins/downloader.py
@@ -1,9 +1,5 @@
 import requests
 import os
-import asyncio
-import json
-import glob
-import random
 
 from config import temp, ytm, ytd, CHANNEL
 from database import get_audio, add_audio
@@ -13,34 +9,6 @@ from tgram.types import CallbackQuery, Message
 
 from typing import Union
 
-
-def cookie_txt_file():
-    folder_path = f"{os.getcwd()}/cookies"
-    filename = f"{os.getcwd()}/cookies/logs.csv"
-    txt_files = glob.glob(os.path.join(folder_path, '*.txt'))
-    if not txt_files:
-        raise FileNotFoundError("No .txt files found in the specified folder.")
-    cookie_txt_file = random.choice(txt_files)
-    with open(filename, 'a') as file:
-        file.write(f'Chosen File : {cookie_txt_file}\n')
-    return cookie_txt_file
-
-
-async def check_file_size(link):
-    async def get_format_info(link):
-        proc = await asyncio.create_subprocess_exec(
-            "yt-dlp",
-            "--cookies", cookie_txt_file(),  
-            "-J",
-            link,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE
-        )
-        stdout, stderr = await proc.communicate()
-        if proc.returncode != 0:
-            print(f'Error:\n{stderr.decode()}')
-            return None
-        return json.loads(stdout.decode())
 
 @TgBot.on_callback_query()
 async def on_callback_query(b: TgBot, c: CallbackQuery) -> Union[bool, Message]:
@@ -69,11 +37,8 @@ async def on_callback_query(b: TgBot, c: CallbackQuery) -> Union[bool, Message]:
             song_info["isExplicit"],
         ),
     )
-
     vid_info = ytd.extract_info(
-        "https://youtube.com/watch?v=" + song_info["videoId"],
-        download=True,
-        extra_info={'cookiefile': cookie_txt_file()}  
+        "https://youtube.com/watch?v=" + song_info["videoId"], download=True
     )
     path = ytd.prepare_filename(vid_info)
 
